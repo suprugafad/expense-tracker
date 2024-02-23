a3a97f8686134718f784f5ba5936c0cda0ea76d3
Alexandrina <poidaalesya382@gmail.com>
2024-02-23 12:43:27 +0300

    fix: improve validation in register-user.input

--- src/auth/dto/register-user.input.ts
+++ src/auth/dto/register-user.input.ts
@@ -6,16 +6,16 @@ export class RegisterUserInput {
   @Field()
   @IsString()
   @IsNotEmpty({ message: 'Name cannot be empty' })
+  @Length(0, 50)
   name: string;
 
   @Field()
   @IsEmail()
+  @Length(0, 255)
   email: string;
 
   @Field()
   @IsString()
-  @Length(6, undefined, {
-    message: 'Password must be at least 6 characters long',
-  })
+  @Length(6, 255)
   password: string;
 }
