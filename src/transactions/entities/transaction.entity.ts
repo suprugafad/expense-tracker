7d138019af3638c24e3769b7f4c5e1ace87e5154
Alexandrina <poidaalesya382@gmail.com>
2024-02-27 22:44:34 +0300

    feat: define transaction entity and transaction type enum

---
+++ src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,66 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { User } from 'src/auth/entities/user.entity';
+import { Category } from 'src/categories/entities/category.entity';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { TransactionTypeEnum } from '../transaction-type.enum';
+
+@Entity('transactions')
+@ObjectType('Transaction')
+export class Transaction {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => ID)
+  id: string;
+
+  @Column()
+  @Field()
+  amount: string;
+
+  @Column({
+    type: 'enum',
+    enum: TransactionTypeEnum,
+  })
+  @Field(() => TransactionTypeEnum)
+  type: TransactionTypeEnum;
+
+  @Column({ nullable: true })
+  @Field({ nullable: true })
+  description?: string;
+
+  @Column({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  @Field()
+  date: Date;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP(6)',
+    onUpdate: 'CURRENT_TIMESTAMP(6)',
+  })
+  updated_at: Date;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
+  @Field(() => User)
+  user: User;
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id', referencedColumnName: 'id' })
+  @Field(() => Category)
+  category: Category;
+}
