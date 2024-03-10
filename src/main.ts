73e742fbebd3a84bb7c39d25a379f5f803d5b0b0
Alexandrina <poidaalesya382@gmail.com>
2024-03-10 21:08:41 +0300

    feat: define cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3001',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type,Accept,Authorization',
+    },
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
